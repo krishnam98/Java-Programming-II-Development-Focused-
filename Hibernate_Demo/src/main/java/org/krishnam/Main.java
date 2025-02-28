86cb3c12de1c65a2c708ee04c9c11ab4702ff607
krishnam98 <sonikrishnam98@gmail.com>
2025-02-28 22:24:10 +0530

    Setup classes & Database for Mapping Relations

--- Hibernate_Demo/src/main/java/org/krishnam/Main.java
+++ Hibernate_Demo/src/main/java/org/krishnam/Main.java
@@ -17,25 +17,61 @@ public class Main {
 //        s1.setName("Abhinav");
 //        s1.setAge(23);
 
-//        USING RECORD
-        Record r1 =new Record();
+//        USING RECORD AS TABLE
+//        Record r1 =new Record();
+//
+//        StudentName s=new StudentName();
+//        s.setfName("Krishnam");
+//        s.setmName("Sushil");
+//        s.setlName("Soni");
+//
+//        r1.setRollno(1);
+//        r1.setName(s);
+//        r1.setAge(22);
 
-        StudentName s=new StudentName();
-        s.setfName("Krishnam");
-        s.setmName("Sushil");
-        s.setlName("Soni");
 
-        r1.setRollno(1);
-        r1.setName(s);
-        r1.setAge(22);
+//        Studying Mapping Relations in Hibernate
+//        Setting up student database
+        Student s= new Student();
+        s.setRollNo(1);
+        s.setName("Krishnam");
+        s.setAge(22);
+
+        Student s2= new Student();
+        s2.setRollNo(2);
+        s2.setName("Pranay");
+        s2.setAge(22);
+
+//        Setting up laptop database
+        Laptop l1=new Laptop();
+        l1.setLid(101);
+        l1.setLname("hp");
+
+        Laptop l2=new Laptop();
+        l2.setLid(102);
+        l2.setLname("dell");
+
+        Laptop l3=new Laptop();
+        l3.setLid(103);
+        l3.setLname("macbook");
+
+
+
+
+
+
 
         Configuration config=new Configuration();
 
 //        When Working with student Table use this Class
-//        config.addAnnotatedClass(org.krishnam.Student.class);
+        config.addAnnotatedClass(org.krishnam.Student.class);
+
+//        Laptop Table
+        config.addAnnotatedClass(org.krishnam.Laptop.class);
+
 
 //       When Working with record Table use this class
-        config.addAnnotatedClass(org.krishnam.Record.class);
+//        config.addAnnotatedClass(org.krishnam.Record.class);
 
         config.configure();
 
@@ -46,10 +82,17 @@ public class Main {
 
         Transaction transaction= session.beginTransaction();
 //      Saving the Student
-//       session.persist(s1);
+//       session.persist(s);
+//       session.persist(s2);
+
+        //      Saving the Laptop
+//       session.persist(l1);
+//       session.persist(l2);
+//       session.persist(l3);
+
 
 //        Saving Record
-        session.persist(r1);
+//        session.persist(r1);
 
 //       Updating the student
 //        session.merge(s1);
