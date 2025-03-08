8b7a14e54769cf08fffa5ad9622e13d09433247e
krishnam98 <sonikrishnam98@gmail.com>
2025-03-08 17:22:15 +0530

    Getting Started with Spring Web and created Controllers

---
+++ simpleWebApp/src/main/java/com/krishnam/simpleWebApp/HomeController.java
@@ -0,0 +1,16 @@
+package com.krishnam.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello World!, Welcome to Krishnam's Server";
+    }
+    @RequestMapping("/about")
+    public String about(){
+        return "Krishnam Soni is a Software Developer.";
+    }
+}
